9226d7c471b0b241602b6900f38a9c71799ac8ce
zdecamp <zdecamp@gmail.com>
2020-02-25 21:16:15 -0600

    An itterative Primer Generator by Zach Decamp.
    -- pf1.py --
    
      Main generator program.
    
    -- lastNumber.txt --
    
      Stores last number processed to resume from where you left off,
    and the number of calculations to compute whether it was a prime or not.
    Last number will be separated from the calculation count by a comma,
    "number,calculations"
    
    i.e.  27,4
    
    -- primes.txt --
    
      Simple storage for current list of primes found.
    10 per line, comma separated.

---
+++ pf1.py
@@ -0,0 +1,141 @@
+import math
+import time
+from datetime import datetime
+#from multiprocessing import Pool
+#from functools import partial
+import json
+
+
+def pcalc(rge):
+    global x
+    global xc
+    global c
+
+    #print(x)
+    #print (rge)
+
+    for y in rge :
+        #print(y)
+        if (x % y == 0) :
+            #print("Failed with " + str(y) + " in " + str(c) + " calcs")
+            nfile = open('lastNumber.txt', 'w', encoding='utf-8')
+            write = str(x) + "," + str(c)
+            nfile.write(write)
+            nfile.close()
+            break
+
+        if len(str(x)) > 20 :
+            if(c % 100000000000 == 0 and c <999999999999):
+                print(str(int(c*0.000000001)) + "B calculations processed ")
+
+            elif(c % 10000000000 == 0 and c < 99999999999):
+                print(strrge(int(c*0.000000001)) + "B calculations processed")
+
+            elif(c % 1000000000 == 0 and c < 9999999999):
+                print(str(int(c*0.000000001)) + "B calculations processed")
+
+            elif(c % 100000000 == 0 and c < 999999999):
+                print(str(int(c*0.000001)) + "M calculations processed ")
+
+            #if(c % 10000000 == 0 and c < 99999999):
+                #print(str(int(c*0.000001)) + "M calculations processed")
+                #print(x)
+                #print(y)
+        #print("Prime = " + str(x) + " with " + str(c) + " calculations")
+        c=c+1
+
+    else:
+
+        if len(str(x)) <= 5 :
+            if c % 10 == 0 :
+        #print(str(c)+" calc "+str(y))
+                print("Prime = " + str(x) + " with " + str(c) + " calculations")
+        elif 5 < len(str(x)) <= 7  :
+            if c % 50 == 0 :
+                #print(str(c)+" calc "+str(y))
+                print("Prime = " + str(x) + " with " + str(c) + " calculations")
+        elif 7 < len(str(x)) <= 14  :
+            if c % 250 == 0 :
+                #print(str(c)+" calc "+str(y))
+                print("Prime = " + str(x) + " with " + str(c) + " calculations")
+        elif 14 < len(str(x)) <= 15 :
+            if c % 100 == 0 :
+                #print(str(c)+" calc "+str(y))
+                print("Prime = " + str(x) + " with " + str(c) + " calculations")
+        elif 15 < len(str(x)) <= 20 :
+            if c % 10 == 0 :
+                #print(str(c)+" calc "+st)
+                print("Prime = " + str(x) + " with " + str(c) + " calculations")
+        elif len(str(x)) > 20 :
+            print("Prime = " + str(x) + " with " + str(c) + " calculations")
+
+        if xc%10 == 0:
+            f = open('primes.txt', 'a')
+            lbr = "\n"
+            f.write(lbr)
+
+        f = open('primes.txt', 'a')
+        prime = str(x) + ","
+        f.write(prime)
+        xc = xc+1
+#        return x
+
+
+
+try:
+    file = open('lastNumber.txt', 'r')
+    info = (file.readline())
+    splift = info.split(",",1)[0]
+    clinfo = info.split(",",1)[1]
+    print("previous number checked " + str(splift))
+    print("Calculations for last number: " + str(clinfo))
+
+except IOError :
+    print()
+
+ui1 = int(input("How many numbers would you like to process?: "))
+#start = 3
+start = int(splift) + 2
+stop = start + int(ui1)
+
+plist = []
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+tstart = time.time()
+xc = 0
+
+print(current_time)
+
+x = start
+sqr = int(math.sqrt(x))
+rge = range(3,sqr,2)
+
+
+for x in range(start, stop +1):
+
+    s = int(str(x)[-1])
+#print(s)
+    if s in (1,3,7,9) :
+        #print(s)
+        if len(str(x)) > 20 :
+            print("Testing " + str(x))
+
+        sqr = int(math.sqrt(x))
+
+        c = 1
+
+        pcalc(rge)
+
+
+tend = time.time()
+
+
+
+
+#print(plist)
+print("Completed in " + str(round((tend-tstart),3)/60) + " minutes")
+file = open('lastNumber.txt', 'r')
+info = (file.readline())
+splift = info.split(",",1)[0]
+clinfo = info.split(",",1)[1]
+print("last number checked " + str(splift))
